31613687be2f74f607240f2fefdcbfb25b1f8747
rendre <rendre90@gmail.com>
2021-08-17 03:24:03 +0700

    CHORE : add navigation interfaces

---
+++ Assets/Prototype Lsw Programming Interview/Scripts/System/Navigation/IRotate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrototypeLSWProgrammingInterview.System.Navigation{
+    public interface IRotate
+    {
+            
+    }
+}
