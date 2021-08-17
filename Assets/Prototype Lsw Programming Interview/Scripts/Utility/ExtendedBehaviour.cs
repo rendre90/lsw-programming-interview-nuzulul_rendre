4625338bd7fd0a200559a1a1a42257c6bc23c143
rendre <rendre90@gmail.com>
2021-08-18 01:32:59 +0700

    CHORE : add Extended Behaviour for find nearest collider and filter collider

---
+++ Assets/Prototype Lsw Programming Interview/Scripts/Utility/ExtendedBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+namespace PrototypeLSWProgrammingInterview.Utility{
+    public static class ExtendedBehaviour
+    {
+        public static Collider2D FindNearestCollider(this Transform transform, Collider2D[] colliders){
+            float nearestDis = Mathf.Infinity;
+            Collider2D nearestCollider = null;
+            for(int i = 0; i < colliders.Length; i++){
+                float currentDis = Vector3.Distance(transform.position, colliders[i].transform.position);
+                if(currentDis < nearestDis){
+                    nearestDis = currentDis;
+                    nearestCollider = colliders[i];
+                }
+            }
+
+            return nearestCollider;
+        }
+
+        public static Collider2D FindNearestCollider(this Transform transform, List<Collider2D> colliders){
+            float nearestDis = Mathf.Infinity;
+            Collider2D nearestCollider = null;
+            for(int i = 0; i < colliders.Count; i++){
+                float currentDis = Vector3.Distance(transform.position, colliders[i].transform.position);
+                if(currentDis < nearestDis){
+                    nearestDis = currentDis;
+                    nearestCollider = colliders[i];
+                }
+            }
+
+            return nearestCollider;
+        }
+        public static Collider2D[] FilterCollider(this Collider2D[] colliders, Predicate<Collider2D> p = null){
+            return p == null ? colliders : Array.FindAll(colliders, p);
+        }
+    }
+}
