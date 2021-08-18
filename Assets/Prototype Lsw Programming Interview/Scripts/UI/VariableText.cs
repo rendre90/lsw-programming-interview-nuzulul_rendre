298e6ef59b7d40b8036dbdbb7d5734112a62b8e0
rendre <rendre90@gmail.com>
2021-08-19 03:55:37 +0700

    FEAT : add variable text to variable in text

---
+++ Assets/Prototype Lsw Programming Interview/Scripts/UI/VariableText.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrototypeLSWProgrammingInterview.Utility.VariableReference;
+using TMPro;
+
+namespace PrototypeLSWProgrammingInterview.UI
+{
+    public class VariableText : MonoBehaviour
+    {
+        [SerializeField] BaseVariableReference value;
+        [SerializeField] TMP_Text valueText;
+        [SerializeField] string initialText;
+
+        void OnEnable(){
+            value.OnUpdateVariable += UpdateValue;
+            UpdateValue();
+        }
+
+        void Start(){
+            UpdateValue();
+        }
+
+    
+        void OnDisable(){
+            value.OnUpdateVariable -= UpdateValue;
+        }
+
+        void UpdateValue(){
+            valueText.text = $""+initialText + value.ToString();
+        } 
+    }
+}
+
