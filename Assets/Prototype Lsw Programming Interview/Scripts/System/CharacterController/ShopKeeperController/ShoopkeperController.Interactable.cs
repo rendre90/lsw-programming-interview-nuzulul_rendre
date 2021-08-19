3389390c41f01b5fec7f7a29b68a2498593a98af
rendre <rendre90@gmail.com>
2021-08-20 01:48:06 +0700

    FEAT : player can buy item

--- Assets/Prototype Lsw Programming Interview/Scripts/System/CharacterController/ShopKeeperController/ShoopkeperController.Interactable.cs
+++ Assets/Prototype Lsw Programming Interview/Scripts/System/CharacterController/ShopKeeperController/ShoopkeperController.Interactable.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using PrototypeLSWProgrammingInterview.System.Interaction;
 using PrototypeLSWProgrammingInterview.System.Interaction.InteractableSign;
-
+using PrototypeLSWProgrammingInterview.Utility.MenuLink;
 namespace PrototypeLSWProgrammingInterview.System.CharacterController.ShopKeeperController{
     public partial class ShoopkeperController : BaseCharacterController, IInteractable 
     {
         [SerializeField] GameObject signObj;
+        [SerializeField] MenuLink shopLink;
         public IInteractableSign interactableSign{get; private set;}
 
         void InitializeInteractable(){
             interactableSign = new InteractableSignWithImage(signObj);
         }
         void IInteractable.Interacted(){
-            Debug.Log("Interacted");
+            shopLink.InvokeActionLink();
         }
     }
 }
